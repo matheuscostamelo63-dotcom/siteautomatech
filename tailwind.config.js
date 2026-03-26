cbd6fc67aa08812c7c89dfa49f09c9438fffffaa
matheusz <matheuscostamelo63@gmail.com>
2026-03-26 14:10:15 -0300

    Modernize typography with Outfit and Inter fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,7 @@ export default {
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
-        heading: ["Space Grotesk", "sans-serif"],
+        heading: ["Outfit", "sans-serif"],
       },
     },
   },
